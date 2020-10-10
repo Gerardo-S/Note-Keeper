a65ebae684f7aea53d1de15ba93113703e5f5bda
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-09 17:27:23 -0700

    fixed link to stylesheet

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,21 @@
+// noteData links to data json file
 const noteData = require("../db/db");
 
 module.exports = function (app) {
-    // This code will display all notes saved to db.json file
-    app.get("/api/notes", (req, res) => {
-      res.json(noteData);
-    });
-  
-    app.post("/api/notes", (req, res) => {
-        console.log(req.body);
-      
-        noteData.push(req.body);
-        res.json(noteData);
-       
-    });
-  
-    
-  };
-  
+
+  // This code will display all notes saved to db.json file
+  app.get("/api/notes", (req, res) => {
+    res.json(noteData);
+  });
+
+
+  // This code will retrieve new note and post to data file
+  app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    noteData.push(newNote);
+    res.json(noteData);
+    console.log("New note: " + newNote.title)
+  });
+
+
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,8 @@ module.exports = function (app) {
   // Below code handles when users navigate to a page. (e.g. follow a link or
   // enter a url in the address bar) The client receives an HTML document to
   // show the user as a response for each of these routes.
+
+
   app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
@@ -11,6 +13,7 @@ module.exports = function (app) {
    // If no matching route is found default to the home page.
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
+
   });
 
 };
